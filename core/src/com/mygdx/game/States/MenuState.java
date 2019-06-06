56e70c0f777c1063520f7fb8ca8e28195d133cff
Shen <gpdgpa@gmail.com>
2019-06-06 23:24:50 +0800

    吸毒佬 1-3 already to add background and play button

---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+    public MenuState(GameStateManager gam) {
+        super(gam);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
